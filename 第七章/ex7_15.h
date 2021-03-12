1aba0c333cb708e6bae477d1de88d229c48cebb0
Sugar <45682h@163.com>
2021-03-13 02:48:17 +0800

    add chapter 7

--- 第七章/ex7_15.h
+++ 第七章/ex7_15.h
@@ -1,13 +1,18 @@
-#ifndef ex7_9_h
-#define ex7_9_h
+#ifndef ex7_15_h
+#define ex7_15_h
 
 #include <string>
 #include <iostream>
 
 struct Person
 {
+    Person() = default;
+    Person(std::string &na, std::string ad) : name(na), address(ad) {}
+    Person(std::istream &is) { read(is, *this); }
+
     std::string const &getname() const { return name; }
     std::string const &getAddress() const { return address; }
+
     std::string name;
     std::string address;
 };
--- 第七章/ex7_22.h
+++ 第七章/ex7_22.h
@@ -1,13 +1,23 @@
-#ifndef ex7_9_h
-#define ex7_9_h
+#ifndef ex7_15_h
+#define ex7_15_h
 
 #include <string>
 #include <iostream>
 
-struct Person
+class Person
 {
+    friend std::istream &read(std::istream &is, Person &person);
+    friend std::ostream &print(std::ostream &os, Person &person);
+
+public:
+    Person() = default;
+    Person(std::string &na, std::string ad) : name(na), address(ad) {}
+    Person(std::istream &is) { read(is, *this); }
+
     std::string const &getname() const { return name; }
     std::string const &getAddress() const { return address; }
+
+private:
     std::string name;
     std::string address;
 };
